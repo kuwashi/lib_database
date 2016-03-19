1c04e1dfe947fdf9ee84ea655f496503b51ec0a7
kuwashi <ka65@njit.edu>
2016-03-19 17:35:55 -0400

    second commit

--- library.sql
+++ library.sql
@@ -14,6 +14,171 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Author`
+--
+
+DROP TABLE IF EXISTS `Author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Author` (
+  `FirstName` varchar(20) NOT NULL,
+  `LastName` varchar(20) NOT NULL,
+  `DateOfBirth` date NOT NULL,
+  PRIMARY KEY (`FirstName`,`LastName`,`DateOfBirth`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Author`
+--
+
+LOCK TABLES `Author` WRITE;
+/*!40000 ALTER TABLE `Author` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Author_Book`
+--
+
+DROP TABLE IF EXISTS `Author_Book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Author_Book` (
+  `BookID` int(11) NOT NULL,
+  `AuthorFirstName` varchar(20) NOT NULL,
+  `AuthorLastName` varchar(20) NOT NULL,
+  `AuthorDateOfBirth` date NOT NULL,
+  PRIMARY KEY (`BookID`,`AuthorFirstName`,`AuthorLastName`,`AuthorDateOfBirth`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Author_Book`
+--
+
+LOCK TABLES `Author_Book` WRITE;
+/*!40000 ALTER TABLE `Author_Book` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Author_Book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Book`
+--
+
+DROP TABLE IF EXISTS `Book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Book` (
+  `BookID` int(11) NOT NULL,
+  `Title` varchar(50) NOT NULL,
+  `ISBN` int(11) NOT NULL,
+  `Year` date NOT NULL,
+  `Publisher` varchar(20) NOT NULL,
+  `Genre` varchar(20) NOT NULL,
+  PRIMARY KEY (`BookID`),
+  UNIQUE KEY `ISBN` (`ISBN`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Book`
+--
+
+LOCK TABLES `Book` WRITE;
+/*!40000 ALTER TABLE `Book` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Librarian`
+--
+
+DROP TABLE IF EXISTS `Librarian`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Librarian` (
+  `LibID` int(11) NOT NULL,
+  `FirstName` varchar(20) NOT NULL,
+  `LastName` varchar(20) NOT NULL,
+  `DateOfBirth` date NOT NULL,
+  `Role` varchar(20) NOT NULL,
+  `Address` varchar(50) NOT NULL,
+  `Email` varchar(35) NOT NULL,
+  `Eusername` varchar(20) NOT NULL,
+  `Epassword` varchar(20) NOT NULL,
+  PRIMARY KEY (`LibID`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Librarian`
+--
+
+LOCK TABLES `Librarian` WRITE;
+/*!40000 ALTER TABLE `Librarian` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Librarian` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Reservation`
+--
+
+DROP TABLE IF EXISTS `Reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Reservation` (
+  `ReservationID` int(11) NOT NULL AUTO_INCREMENT,
+  `BookID` int(11) NOT NULL,
+  `Issuedate` date NOT NULL,
+  `Returndate` date NOT NULL,
+  `StudentID` int(11) NOT NULL,
+  PRIMARY KEY (`ReservationID`,`BookID`,`StudentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Reservation`
+--
+
+LOCK TABLES `Reservation` WRITE;
+/*!40000 ALTER TABLE `Reservation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Student`
+--
+
+DROP TABLE IF EXISTS `Student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Student` (
+  `StudentID` int(11) NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(20) NOT NULL,
+  `LastName` varchar(20) NOT NULL,
+  `DateOfBirth` date NOT NULL,
+  `PhoneNum` int(11) NOT NULL,
+  `Address` varchar(50) NOT NULL,
+  `Email` varchar(35) NOT NULL,
+  `Susername` varchar(20) NOT NULL,
+  `SPassword` varchar(20) NOT NULL,
+  PRIMARY KEY (`StudentID`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Student`
+--
+
+LOCK TABLES `Student` WRITE;
+/*!40000 ALTER TABLE `Student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Student` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +189,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-18 23:43:13
+-- Dump completed on 2016-03-19 17:28:09
